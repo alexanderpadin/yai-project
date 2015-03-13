10026d57d47ba994fff4e949304812f5b23c882b
Alexander Padin <alexander.padin91@gmail.com>
2015-03-13 16:43:59 -0400

    Add clients fixed

--- dependencies/modules/controlPanel.js
+++ dependencies/modules/controlPanel.js
@@ -4,6 +4,7 @@
 	app.controller('controlPanelCtrl', function($scope, $http){
     	
     	$scope.clients = [];
+        $scope.servicios = [];
         
         $scope.getAllItems = function(queryLimit, querySkip, first) {
 
@@ -21,6 +22,7 @@
                             $scope.getAllItems(queryLimit, querySkip, first);
                         } else {
                             //Data ready.
+                            $scope.getServices();
                         }
                     } else {
                         var newQ = data.results;
@@ -32,6 +34,7 @@
                             $scope.getAllItems(queryLimit, querySkip, first);
                         } else {
                             //Data Ready
+                            $scope.getServices();
                         }
                     }
                 })
@@ -39,6 +42,20 @@
                     alert("Error");
                 });
         };
+
+        $scope.getServices = function() {
+             $http({method : 'GET',
+                url : 'https://api.parse.com/1/classes/services', 
+                headers: { 'X-Parse-Application-Id':'eTTIg8J0wMN5GYb4ys3PH152xuMK8WdpNUy8u8S8','X-Parse-REST-API-Key':'VmzCpgQRTiP4UYNEvIbeOiOEK8WB3ruA0WnAmmBU'},
+                params: {limit:1000},
+                
+                }).success(function(data, status) {
+                        $scope.servicios = data.results;
+                    })
+                .error(function(data, status) {
+                    console.log("Error:" + data + " Status:" + status);
+                });
+        }
 
         $scope.updateItem = function(ID, info) {
             $http({method : 'PUT',
@@ -53,39 +70,106 @@
                 });
         };
 
-        $scope.addItem = function(nombre, expiracion, deuda, metodo, pago, pueblo, servicio, unidades, telefono, pueblo, pago, metodo, deuda) {
-
-            var info = {
-                "nombre" : nombre, 
-                "expiracion" : expiracion, 
-                "deuda" : deuda, 
-                "metodo" : metodo, 
-                "pago" : pago, 
-                "pueblo" : pueblo, 
-                "servicio" : servicio, 
-                "unidades" : unidades, 
-                "telefono" : telefono, 
-                "pueblo" : pueblo, 
-                "pago" : pago, 
-                "metodo" : metodo, 
-                "deuda" : deuda
-            };
-
-            $http({method : 'POST',
-                url : 'https://api.parse.com/1/classes/clientes/clients', 
-                headers: {'X-Parse-Application-Id':'eTTIg8J0wMN5GYb4ys3PH152xuMK8WdpNUy8u8S8','X-Parse-REST-API-Key':'VmzCpgQRTiP4UYNEvIbeOiOEK8WB3ruA0WnAmmBU'},
-                data: info
+        $scope.addItem = function(Nombre, Expiracion, Deuda, Metodo, Pago, Pueblo, Servicio, Unidades, Telefono, Clave, Activo) {
+            $http({
+                    method : 'POST',
+                    url : 'https://api.parse.com/1/classes/clients', 
+                    headers: { 
+                        'X-Parse-Application-Id':'eTTIg8J0wMN5GYb4ys3PH152xuMK8WdpNUy8u8S8',
+                        'X-Parse-REST-API-Key':'VmzCpgQRTiP4UYNEvIbeOiOEK8WB3ruA0WnAmmBU'
+                    },
+                    data: {
+                        nombre: Nombre,
+                        expiracion: Expiracion,
+                        deuda: parseInt(Deuda),
+                        metodo: Metodo,
+                        pago: parseInt(Pago),
+                        pueblo: Pueblo,
+                        servicio: Servicio,
+                        unidades: parseInt(Unidades),
+                        telefono: Telefono,
+                        clave: Clave,
+                        activo: Activo
+                    }
                 }).success(function(data, status) {
-                    alert(data);
+                     $scope.getAllItems(1000,0, true);
                 })
                 .error(function(data, status) {
-                    alert("Error");
+                    alert("Se ha producido un error creando el nuevo cliente.");
                 });
         };
 
-        $scope.getAllItems(1000,0, true);
+        $scope.validateDate = function(dateString) {
+            if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
+                return false;
 
-    	
+            var parts = dateString.split("/");
+            var day = parseInt(parts[1], 10);
+            var month = parseInt(parts[0], 10);
+            var year = parseInt(parts[2], 10);
+
+            if(year < 1000 || year > 3000 || month == 0 || month > 12)
+                return false;
+
+            var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+            if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+                monthLength[1] = 29;
+
+            return day > 0 && day <= monthLength[month - 1];
+        };
+
+        $scope.validatesAddInputs = function() {
+
+            var nombre = document.getElementById('add_nombre').value;
+            var expiracion = document.getElementById('add_expiracion').value;
+            var metodo = document.getElementById('add_metodo').value;
+            var pago = document.getElementById('add_pago').value;
+            var pueblo = document.getElementById('add_pueblo').value;
+            var servicio = $scope.servicios[parseInt(document.getElementById('add_servicio').value)].service;
+            var unidades = document.getElementById('add_unidades').value;
+            var telefono = document.getElementById('add_telefono').value;
+            var clave = "";
+            var activo = true;
+            var deuda = 0;
+
+            if(!((nombre == null || nombre == "") 
+                || (expiracion == null || expiracion == "")
+                || (metodo == null || metodo == "")
+                || (pago == null || pago == "")
+                || (servicio == null || servicio == ""))){
+
+                if($scope.validateDate(expiracion)) {
+
+                    $scope.addItem(nombre, expiracion, deuda, metodo, pago, pueblo, servicio, unidades, telefono, clave, activo);
+                   
+                    var nombre = document.getElementById('add_nombre').value = "";
+                    var expiracion = document.getElementById('add_expiracion').value = "";
+                    var metodo = document.getElementById('add_metodo').value = "Selecciona";
+                    var pago = document.getElementById('add_pago').value = "";
+                    var pueblo = document.getElementById('add_pueblo').value = "";
+                    var servicio = document.getElementById('add_servicio').value = "Selecciona";
+                    var unidades = document.getElementById('add_unidades').value = "";
+                    var telefono = document.getElementById('add_telefono').value = "";
+
+                } else {
+                    alert("Fecha de expiracion invalida.");
+                }
+            } else {
+                alert("Llene todo los blancos requeridos.");
+            }
+        };
+
+        $scope.updatePrice = function(){
+            if(!(document.getElementById('add_servicio').value == null || document.getElementById('add_servicio').value == "")) {
+                document.getElementById('add_pago').value = ($scope.servicios[parseInt(document.getElementById('add_servicio').value)].price * parseInt(document.getElementById('add_unidades').value));
+            } else {
+                document.getElementById('add_pago').value = 0;
+            }
+        };
+
+        $scope.getAllItems(1000,0, true);
+        
   	});
 
     //Filtro para los quipos en la tabla
@@ -98,6 +182,4 @@
 	   return filtered;
 	  };
 	});
-
-   
 })();
